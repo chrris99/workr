9cdae4131431ec74d4ac30f9711c524f2900826c
chrris99 <conforti.christian@gmail.com>
2022-11-19 13:01:44 +0100

    implement jwt provider

---
+++ src/Workr.Infrastructure/Auth/JwtOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace Workr.Infrastructure.Auth;
+
+/// <summary>
+/// Represents the JWT configuration settings.
+/// </summary>
+public sealed class JwtOptions
+{
+    public const string Jwt = "Jwt";
+
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public string Secret { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="JwtOptions"/> class.
+    /// </summary>
+    public JwtOptions()
+    {
+        Issuer = string.Empty;
+        Audience = string.Empty;
+        Secret = string.Empty;
+    }
+}
---
+++ src/Workr.Infrastructure/Auth/JwtProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Workr.Application.Abstractions;
+using Workr.Domain.Entities;
+
+namespace Workr.Infrastructure.Auth;
+
+public sealed class JwtProvider : IJwtProvider
+{
+    private readonly JwtOptions _options;
+    
+    public JwtProvider(IOptions<JwtOptions> options)
+    {
+        _options = options.Value;
+    }
+    
+    /// <inheritdoc cref="Create"/>
+    public string Create(User user)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Secret));
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Issuer = _options.Issuer,
+            Audience = _options.Audience,
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new ("userId", user.Id),
+                new ("email", user.Email)
+            }),
+            // TODO: Add expiration date
+            SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256)
+        };
+        
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
