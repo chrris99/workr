ab54ca9607bdf7ae774c9e771767008e6e6c36a3
chrris99 <conforti.christian@gmail.com>
2022-11-19 13:01:02 +0100

    add token provider abstraction

---
+++ src/Workr.Application/Abstractions/IJwtProvider.cs
@@ -0,0 +1,16 @@
+﻿using Workr.Domain.Entities;
+
+namespace Workr.Application.Abstractions;
+
+/// <summary>
+/// JWT Provider interface.
+/// </summary>
+public interface IJwtProvider
+{
+    /// <summary>
+    /// Creates the JWT for the specified user.
+    /// </summary>
+    /// <param name="user">The user.</param>
+    /// <returns>The JWT for the specified user.</returns>
+    string Create(User user);
+}
