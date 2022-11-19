7ba0716209e52aaeb11e8d4148762f3a2c03a5f3
chrris99 <conforti.christian@gmail.com>
2022-11-19 12:59:51 +0100

    add controllers

---
+++ src/Workr.Api/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Workr.Application.Contracts.Auth;
+using Workr.Application.Services;
+
+namespace Workr.Api.Controllers;
+
+[ApiController]
+[Route(Routes.Auth.Base)]
+public sealed class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AuthController"/> class.
+    /// </summary>
+    /// <param name="authService"></param>
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+    
+    [HttpPost]
+    [Route(Routes.Auth.Register)]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    {
+        if (!ModelState.IsValid) return BadRequest();
+
+        var result = await _authService.RegisterAsync(request.Email, request.Password);
+
+        return result.IsSuccess ? Ok(result.Value) : BadRequest(result.Error);
+    }
+
+    [HttpPost]
+    [Route(Routes.Auth.Login)]
+    public async Task<IActionResult> Login()
+    {
+        if (!ModelState.IsValid) return BadRequest();
+        
+        // Model state is valid, implement logic
+        return Ok();
+    }
+}
---
+++ src/Workr.Api/Controllers/ExerciseController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace Workr.Api.Controllers;
+
+[Route("/api/[controller]")]
+public sealed class ExerciseController : ApiController
+{
+    [HttpGet]
+    public IResult GetExercise()
+    {
+        return Results.Ok("Ping");
+    }
+}
