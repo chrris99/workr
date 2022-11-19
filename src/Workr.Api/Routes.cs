edf16168938370ada2a8cea4d5187d2955a777ff
chrris99 <conforti.christian@gmail.com>
2022-11-19 12:59:31 +0100

    add api project

---
+++ src/Workr.Api/Routes.cs
@@ -0,0 +1,23 @@
+﻿namespace Workr.Api;
+
+public static class Routes
+{
+    private const string Root = "api";
+
+    public static class Auth
+    {
+        public const string Base = $"{Root}/auth";
+        
+        public const string Register = "register";
+        public const string Login = "login";
+    }
+    
+    public static class Exercise
+    {
+        public const string Base = $"{Root}/exercise";
+        
+        public const string Create = "";
+        public const string Update = "";
+        public const string Delete = "";
+    }
+}
