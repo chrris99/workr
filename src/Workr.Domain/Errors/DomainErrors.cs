23e895d1d3ea5597d22ee932a896c47605a6f938
chrris99 <conforti.christian@gmail.com>
2022-11-19 12:57:29 +0100

    add predefined domain errors

---
+++ src/Workr.Domain/Errors/DomainErrors.cs
@@ -0,0 +1,12 @@
+﻿using Workr.Domain.Abstractions;
+
+namespace Workr.Domain.Errors;
+
+/// <summary>
+/// Contains the domain errors.
+/// </summary>
+public static class DomainErrors
+{
+    public static Error UserAlreadyExists =>
+        new ("User.AlreadyExists", "User with the given email address already exists.");
+}
